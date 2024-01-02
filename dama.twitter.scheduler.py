44cc9563f4eba3be6140eb4d4db638c20195f8c0
2^64 <102126589+drivewave@users.noreply.github.com>
2024-01-03 00:10:20 +0100

    Add files via upload

--- dama.twitter.scheduler.py
+++ dama.twitter.scheduler.py
@@ -2,9 +2,9 @@ from funcs import twitterScheduler
 from dama import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('margindays', help='Indicates the margin of days between each post.')
-parser.add_argument('hour', help='Indicates the hour of publication.')
-parser.add_argument('minutes', help='Indicates the minutes of publication.')
+parser.add_argument('margindays', help='')
+parser.add_argument('hour', help='')
+parser.add_argument('minutes', help='')
 args = parser.parse_args()
 
 twitterScheduler(int(args.margindays), int(args.hour), int(args.minutes))
